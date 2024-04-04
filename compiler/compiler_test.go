92005fc95e52629bd1c6e0f4c24eee6c88832db0
Joseph LoManto <gamerjoe007@gmail.com>
2024-04-03 22:09:07 -0400

    Compiler: Hello Bytecode!

---
+++ compiler/compiler_test.go
@@ -0,0 +1,102 @@
+package compiler
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/code"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+	"testing"
+)
+
+type compilerTestCase struct {
+	input                string
+	expectedConstants    []any
+	expectedInstructions []code.Instructions
+}
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase) {
+	t.Helper()
+	for _, tt := range tests {
+		program := parse(tt.input)
+		compiler := New()
+		err := compiler.Compile(program)
+		if err != nil {
+			t.Fatalf("compile error: %s", err)
+		}
+		bytecode := compiler.Bytecode()
+		err = testInstructions(tt.expectedInstructions, bytecode.Instructions)
+		if err != nil {
+			t.Fatalf("testInstructions failed: %s", err)
+		}
+		err = testConstants(t, tt.expectedConstants, bytecode.Constants)
+		if err != nil {
+			t.Fatalf("testConstants failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testInstructions(expected []code.Instructions, actual code.Instructions) error {
+	concatted := concattedInstructions(expected)
+	if len(actual) != len(concatted) {
+		return fmt.Errorf("wrong instruction length.\nwant=%q\ngot=%q", concatted, actual)
+	}
+	return nil
+}
+
+func concattedInstructions(s []code.Instructions) code.Instructions {
+	out := code.Instructions{}
+	for _, ins := range s {
+		out = append(out, ins...)
+	}
+	return out
+}
+
+func testConstants(t *testing.T, expected []any, actual []object.Object) error {
+	if len(expected) != len(actual) {
+		return fmt.Errorf("wrong number of constants. got=%q want=%q", len(actual), len(expected))
+	}
+	for i, constant := range expected {
+		switch constant := constant.(type) {
+		case int:
+			err := testIntegerObject(int64(constant), actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject falied: %s", i, err)
+			}
+		}
+	}
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got%T (%+v)", actual, actual)
+	}
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+	}
+	return nil
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "1 + 2",
+			expectedConstants: []any{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpAdd),
+			},
+		},
+	}
+	runCompilerTests(t, tests)
+}
